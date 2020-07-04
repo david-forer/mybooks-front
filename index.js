ec1cf259efc1d24210c7f0b2f50a4c9a336881c6
david-forer <david.j.forer@gmail.com>
2020-07-04 14:37:30 +0900

    added get fetch, styles

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("i work")
+const bookUrl = "http://localhost:3000/api/v1/books";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log('Dom is loaded')
+  getBooks();
+});
+
+function getBooks() {
+  fetch(bookUrl)
+    .then((response) => response.json())
+    .then((books) => {
+      books.data.forEach((book) => {
+        const bookMarkup = `
+            <div data-id=${book.id}>
+            <li>
+                <h3>${book.attributes.title}</h3>
+                    <p>${book.attributes.author}</p>
+                    <p>${book.attributes.description}</p>                      
+                    <p>${book.attributes.image_url}</p>
+                    <p>${book.attributes.genre.name}</p>
+            </li>
+            <button data-id=${book.id}>edit</button> <button data-id=${book.id}>delete</button>
+            </div><br><br>`;
+        document.querySelector("#notes-list").innerHTML += bookMarkup;
+      });
+    });
+}
